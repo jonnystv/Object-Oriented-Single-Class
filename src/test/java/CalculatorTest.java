d98c85600331a623f3407f8b0dbd71e5b5a76e1e
jonnystv <jonathan@darefoundation.com>
2021-02-08 15:52:18 +0000

    add test failed

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,2 +1,13 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class CalculatorTest {
+
+    Calculator calculator;
+
+    @Test
+    public void canAdd() {
+        assertEquals(4, calculator.add(2, 2));
+    }
 }
